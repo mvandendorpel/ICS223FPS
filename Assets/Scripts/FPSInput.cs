d656b640d1e3f7f022b6f34eb8de8e38bd2424e0
mvandendorpel <chaosengel@gmail.com>
2022-01-31 19:43:42 -0800

    Completion of Lab3 - Added the player and movement

--- Assets/Scripts/FPSInput.cs
+++ Assets/Scripts/FPSInput.cs
@@ -2,25 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Hole : MonoBehaviour
+public class FPSInput : MonoBehaviour
 {
+    private float speed = 9.0f;
     // Start is called before the first frame update
     void Start()
     {
+
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0.0f,  Input.GetAxis("Vertical"));
+        transform.Translate(movement * speed * Time.deltaTime, Space.Self);
         
-    }
-
-    void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.tag == "Ball")
-        {
-            Destroy(collision.gameObject);
-        }
     }
 }
---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    // enum to set values by name instead of number. 
+    // makes code more readable! 
+    public enum RotationAxes
+    {
+        MouseXAndY,
+        MouseX,
+        MouseY
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public RotationAxes axes = RotationAxes.MouseXAndY;
+    public float sensitivityHoriz = 9.0f;
+    public float sensitivityVert = 9.0f;
+    public float minVert = -45.0f;
+    public float maxVert = 45.0f;
+
+    private float rotationX = 0.0f;
+    void Update()
+    {
+        
+        
+
+        if (axes == RotationAxes.MouseX)
+        {
+            transform.Rotate(Vector3.up * Input.GetAxis("Mouse X") * sensitivityHoriz);
+
+        }
+        else if (axes == RotationAxes.MouseY)
+        {
+            rotationX -= Input.GetAxis("Mouse Y") * sensitivityVert;
+            rotationX = Mathf.Clamp(rotationX, minVert, maxVert);
+            transform.localEulerAngles = new Vector3(rotationX, 0, 0);
+
+        }
+        else
+        {
+            rotationX -= Input.GetAxis("Mouse Y") * sensitivityVert;
+            rotationX = Mathf.Clamp(rotationX, minVert, maxVert);
+            float deltaHoriz = Input.GetAxis("Mouse X") * sensitivityHoriz;
+            float rotationY = transform.localEulerAngles.y + deltaHoriz;
+
+
+
+            transform.localEulerAngles = new Vector3(rotationX, rotationY, 0);
+        }
+        
+    }
+}
