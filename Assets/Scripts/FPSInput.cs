f9a0877a43acaa38b2d2a5e950f620010caff921
mvandendorpel <chaosengel@gmail.com>
2022-02-07 21:20:48 -0800

    Lab 4
    Completion of Lab 4
    - Added new enemy model
    - Created Script for ray gun
    - Added indicator for missed shot
    - Added script for enemy to react to shot

--- Assets/Scripts/FPSInput.cs
+++ Assets/Scripts/FPSInput.cs
@@ -5,18 +5,25 @@ using UnityEngine;
 public class FPSInput : MonoBehaviour
 {
     private float speed = 9.0f;
+    private float gravity = -9.8f;
+    private CharacterController charController;
     // Start is called before the first frame update
     void Start()
     {
-
-        
+        charController = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0.0f,  Input.GetAxis("Vertical"));
-        transform.Translate(movement * speed * Time.deltaTime, Space.Self);
+        float deltaX = Input.GetAxis("Horizontal");
+        float deltaY = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(deltaX, 0.0f,  deltaY);
+        movement = Vector3.ClampMagnitude(movement, 1.0f);
+        movement.y = gravity;
+        movement *= speed * Time.deltaTime;
+        movement = transform.TransformDirection(movement);
+        charController.Move(movement);
         
     }
 }
---
+++ Assets/Scripts/RayShooter.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayShooter : MonoBehaviour
+{
+    [SerializeField]
+    private Camera cam;
+    [SerializeField]
+    private int aimSize = 16;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 point = new Vector3(cam.pixelWidth / 2, cam.pixelHeight / 2, 0);
+            Ray ray = cam.ScreenPointToRay(point);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                GameObject hitOjbect = hit.transform.gameObject;
+                ReactiveTarget target = hitOjbect.GetComponent<ReactiveTarget>();
+                if (target != null)
+                {
+                    target.ReactToHit();
+                }
+                else
+                {
+                    // visually indicate where there was a hit 
+                    StartCoroutine(SphereIndicator(hit.point));
+                }
+            }
+        }
+    }
+
+    private void OnGUI()
+    {
+        GUIStyle style = new GUIStyle();
+        style.fontSize = aimSize;
+
+        float posX = cam.pixelWidth / 2 - aimSize / 4;
+        float posY = cam.pixelHeight / 2 - aimSize / 2;
+
+        GUI.Label(new Rect(posX, posY, aimSize, aimSize), "*", style);
+    }
+
+    private IEnumerator SphereIndicator(Vector3 hitPosition)
+    {
+        GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        sphere.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+        sphere.transform.position = hitPosition;
+
+        yield return new WaitForSeconds(1);
+        Destroy(sphere);
+    }
+}
